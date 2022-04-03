340aaba0a725ad1abbbac2532a4d10dcc78fe78d
Robert Udrea <udrearobert999@gmail.com>
2022-04-03 20:35:30 +0300

    Init third person game project

---
+++ Blood-Realms/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform camera;
+    
+    private float speed = 6f;
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
+                turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            var moveDir = Quaternion.Euler(0f, targetAngle, 0f)* Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
